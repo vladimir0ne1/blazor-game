80802f41c5a6efc411771c81426088993ace3da1
Vladimir <1472540+vladimir0ne1@users.noreply.github.com>
2022-02-19 18:04:56 +0300

    Initial setup - create from template (using visual studio): hosted, no auth

---
+++ BlazorGame/Client/Pages/Counter.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorGame.Client.Pages;
+
+public partial class Counter : ComponentBase
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
