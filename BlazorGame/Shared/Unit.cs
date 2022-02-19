86a9be0b42f24618722059956c0a54448c979877
Vladimir <1472540+vladimir0ne1@users.noreply.github.com>
2022-02-19 19:45:54 +0300

    Add Units model(s)

---
+++ BlazorGame/Shared/Unit.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorGame.Shared;
+
+public class Unit
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int Attack { get; set; }
+
+    public int Defense { get; set; }
+
+    public int HitPoints { get; set; } = 100;
+
+    public int BananaCost { get; set; }
+}
---
+++ BlazorGame/Shared/UserUnit.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorGame.Shared;
+
+public class UserUnit
+{
+    public int UserId { get; set; }
+
+    public int UnitId { get; set; }
+
+    public int HitPoints { get; set; }
+}
