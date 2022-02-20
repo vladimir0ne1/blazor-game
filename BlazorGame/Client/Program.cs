c31e72892fc0476d29a8d20832f24defa45f2762
Vladimir <1472540+vladimir0ne1@users.noreply.github.com>
2022-02-20 16:30:18 +0300

    Add army & unit service

--- BlazorGame/Client/Program.cs
+++ BlazorGame/Client/Program.cs
@@ -9,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IBananaService, BananaService>();
+builder.Services.AddScoped<IUnitService, UnitService>();
 
 await builder.Build().RunAsync();
---
+++ BlazorGame/Client/Services/IUnitService.cs
@@ -0,0 +1,35 @@
+﻿using BlazorGame.Shared;
+
+namespace BlazorGame.Client.Services;
+
+public interface IUnitService
+{
+    IList<Unit> Units { get; }
+
+    public IList<UserUnit> MyUnits { get; set; }
+
+    void AddUnit(int unitId);
+}
+
+class UnitService : IUnitService
+{
+    /// <inheritdoc />
+    public IList<Unit> Units { get; } = new List<Unit>
+    {
+        new() { Id = 1, Name = "Knight", Attack = 10, Defense = 10, BananaCost = 100 },
+        new() { Id = 2, Name = "Archer", Attack = 15, Defense = 5, BananaCost = 150 },
+        new() { Id = 3, Name = "Mage", Attack = 20, Defense = 1, BananaCost = 200 },
+    };
+
+    /// <inheritdoc />
+    public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
+
+    /// <inheritdoc />
+    public void AddUnit(int unitId)
+    {
+        var unit = Units.First(unit => unit.Id == unitId);
+        MyUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints });
+        Console.WriteLine($"{unit.Name} was built");
+        Console.WriteLine($"Your army size: {MyUnits.Count}");
+    }
+}
