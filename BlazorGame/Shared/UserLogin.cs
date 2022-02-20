e7b29a8a9c11980d4b96d64715f309a821a1096c
Vladimir <1472540+vladimir0ne1@users.noreply.github.com>
2022-02-20 17:48:53 +0300

    Login form, validation

---
+++ BlazorGame/Shared/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorGame.Shared;
+
+public class UserLogin
+{
+    [Required(ErrorMessage = "Please enter a user name")]
+    public string? UserName { get; set; }
+
+    [Required]
+    public string? Password { get; set; }
+}
